a3e4c1226d42d123222f423e0eb95b228faff17d
yaider <yaiderc19@gmail.com>
2025-07-07 21:21:38 -0500

    feat: setting up database on docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature({
+      type: "postgres",
+      
+
+    })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
