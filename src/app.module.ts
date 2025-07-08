a1abbb95d8b6c6a1da86d486c1e0e8206442bbc3
yaider <yaiderc19@gmail.com>
2025-07-07 22:08:48 -0500

    feat: setting up typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,17 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { ConfigModule } from "@nestjs/config"
 
 @Module({
-  imports: [UserModule,
+  imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forFeature({
+    UserModule,
+    TypeOrmModule.forRoot({
       type: "postgres",
-      
-
+      port: +process.env.DB_HOST!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true
     })
   ],
   controllers: [],
